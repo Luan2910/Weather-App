ad8a69ff2d2191cceeff6b25599868396adcc053
Luan <luanvictor2910@hotmail.com>
2024-10-03 12:44:09 -0300

    First commit - Project complete

---
+++ src/components/iconsRef.js
@@ -0,0 +1,27 @@
+import sun_icon from '../assets/sun.png'
+import cloud_icon from '../assets/cloud.png'
+import clouds_icon from '../assets/clouds.png'
+import rain_icon from '../assets/rain.png'
+import snow_icon from '../assets/snow.png'
+import thunderstorm_icon from '../assets/thunderstorm.png'
+
+const iconsRef = {
+    "01d":sun_icon,
+    "01n":sun_icon,
+    "02d":cloud_icon,
+    "02n":cloud_icon,
+    "03d":cloud_icon,
+    "03n":cloud_icon,
+    "04d":clouds_icon,
+    "04n":clouds_icon,
+    "09d":rain_icon,
+    "09n":rain_icon,
+    "10d":rain_icon,
+    "10n":rain_icon,
+    "11d":thunderstorm_icon,
+    "11n":thunderstorm_icon,
+    "13d":snow_icon,
+    "13n":snow_icon,
+}
+
+export default iconsRef;
